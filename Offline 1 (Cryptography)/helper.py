24f7b1a5f45c1d8829367e04cf0b2679e896f3f4
Sadat Hossain <sadatho2014@gmail.com>
2023-11-30 22:57:13 +0600

    implemented AES-192 and AES-256, did a bit of code refactoring as well

---
+++ Offline 1 (Cryptography)/helper.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+
+def print_hex_string(str):
+    for char in str:
+        if ord(char) < 16:
+            print("0", end="")
+        print(hex(ord(char))[2:].upper(), end=" ")
+    print()
+
+
+def print_hex_array(arr):
+    # arr is 1D
+    for val in arr:
+        if val < 16:
+            print("0", end="")
+        print(hex(val)[2:].upper(), end=" ")
+    print()
+
+
+def print_hex_matrix(mat):
+    for row in mat:
+        print_hex_array(row)
+    print()
+
+
+def print_round_details(round, state_matrix, key_matrix):
+    print("Round", round)
+    print("State Matrix: ")
+    print_hex_matrix(state_matrix)
+    print("Round Key Matrix: ")
+    print_hex_matrix(key_matrix)
+
+
+def print_text_details(title, str, ascii_first):
+    print(title + ":")
+    if ascii_first:
+        print("In ASCII:", str)
+        print("In HEX: ", end="")
+        print_hex_string(str)
+    else:
+        print("In HEX:", str)
+        print("In ASCII: ", end="")
+        print_hex_string(str)
+    print()
+
+
+def string_to_matrix(str):
+    # construct a 4x4 matrix of uint8 data type from the string
+    assert len(str) == 16
+    mat = np.zeros((4, 4), dtype=np.uint8)
+    mat = [[ord(str[i * 4 + j]) for j in range(4)] for i in range(4)]
+    # transpose the matrix to get the column major orientation
+    mat = np.transpose(mat)
+    return mat
+
+
+def matrix_to_string(mat):
+    # transpose the matrix to get the row major orientation
+    mat = np.transpose(mat)
+    str = ""
+    for row in mat:
+        for val in row:
+            str += chr(val)
+    return str
+
+
+def measure_execution_time(func, task_name, *args, **kwargs):
+    import time
+    start = time.time()
+    result = func(*args, **kwargs)
+    end = time.time()
+    return result, (end - start) * 1000
+
+
+def substitute_bytes(arr, src):
+    # arr can be 1D or 2D
+    flat_array = arr.flatten()
+    substituted = np.array([src[val] for val in flat_array])
+    substituted = substituted.reshape(arr.shape)
+    return substituted
--- Offline 1 (Cryptography)/main.py
+++ Offline 1 (Cryptography)/main.py
@@ -1,67 +1,33 @@
 import numpy as np
-from BitVector import *
+from BitVector import BitVector
+from helper import *
 from bitvector_demo import Sbox, InvSbox, Mixer, InvMixer
 
 round_constant = np.array([0x01, 0x00, 0x00, 0x00])
 AES_modulus = BitVector(bitstring='100011011')
-n_rounds = 10
-round_keys = np.array(np.zeros((n_rounds + 1, 4, 4), dtype=np.uint8))
+AES_key_size = 256  # can be 128, 192, or 256 bits
+n_rounds = None
+if AES_key_size == 128:
+    n_rounds = 10
+elif AES_key_size == 192:
+    n_rounds = 12
+else:
+    n_rounds = 14
+# https://engineering.purdue.edu/kak/compsec/NewLectures/Lecture8.pdf (Page 44)
+# will need 44, 52, or 60 words for key expansion (for 128, 192, and 256 bit keys respectively)
+key_expanded = False
+round_keys = np.zeros((64, 4), dtype=np.uint8)
 
 
-def print_hex_string(str):
-    for char in str:
-        if ord(char) < 16:
-            print("0", end="")
-        print(hex(ord(char))[2:].upper(), end=" ")
-    print()
+def expand_key(round):
+    # at the very first expansion round, round will be 1
+    n_words_each_round = AES_key_size // 32
 
+    # copy the last word
+    res = round_keys[n_words_each_round * round - 1].copy()
 
-def print_hex_array(arr):
-    # arr is 1D
-    for val in arr:
-        if val < 16:
-            print("0", end="")
-        print(hex(val)[2:].upper(), end=" ")
-    print()
-
-
-def print_hex_matrix(mat):
-    for row in mat:
-        print_hex_array(row)
-    print()
-
-
-def string_to_matrix(str):
-    # construct a 4x4 matrix of uint8 data type from the string
-    assert len(str) == 16
-    mat = np.zeros((4, 4), dtype=np.uint8)
-    mat = [[ord(str[i * 4 + j]) for j in range(4)] for i in range(4)]
-    # transpose the matrix to get the column major orientation
-    mat = np.transpose(mat)
-    return mat
-
-
-def matrix_to_string(mat):
-    # transpose the matrix to get the row major orientation
-    mat = np.transpose(mat)
-    str = ""
-    for row in mat:
-        for val in row:
-            str += chr(val)
-    return str
-
-
-def substitute_bytes(arr, sbox):
-    # arr can be 1D or 2D
-    flat_array = arr.flatten()
-    substituted = np.array([sbox[val] for val in flat_array])
-    substituted = substituted.reshape(arr.shape)
-    return substituted
-
-
-def expand_key(round, key_matrix):
-    # Circular byte left shift the 4th column (upper shift)
-    res = np.roll(key_matrix[:, -1], -1)
+    # Circular byte left shift
+    res = np.roll(res, -1)
 
     # Byte substitution
     res = substitute_bytes(res, Sbox)
@@ -77,41 +43,69 @@ def expand_key(round, key_matrix):
     # Adding the round constant
     res = np.bitwise_xor(res, round_constant)
 
-    for col in range(4):
-        key_matrix[:, col] = np.bitwise_xor(key_matrix[:, col], res)
-        # print_hex_array(key_matrix[:, col])
-        res = key_matrix[:, col]
+    for i in range(n_words_each_round):
+        round_keys[n_words_each_round * round + i] = np.bitwise_xor(
+            round_keys[n_words_each_round * (round - 1) + i], res)
+        res = round_keys[n_words_each_round * round + i]
 
-    # Round key obtained
+    return
+
+
+def schedule_key(key):
+    assert len(key) >= AES_key_size // 8
+    key = key[:AES_key_size // 8]
+    global round_keys  # array of key matrices
+    global key_expanded
+
+    n_words_each_round = AES_key_size // 32
+    for i in range(len(key)):
+        round_keys[i // 4][i % 4] = ord(key[i])
+
+    # so now the first 4 (or 6 or 8) words of the round key are placed
+
+    ready_words = n_words_each_round
+    round = 1
+
+    while ready_words < (n_rounds + 1) * 4:
+        expand_key(round)
+        ready_words += n_words_each_round
+        round += 1
+
+    key_expanded = True
+    return
+
+
+def generate_key_matrix(round):
+    # the key matrix will be of 4x4 size
+    key_matrix = np.zeros((4, 4), dtype=np.uint8)
+    for i in range(4):
+        key_matrix[:, i] = round_keys[4 * round + i]
     return key_matrix
 
 
-def schedule_key(key, n_rounds):
-    global round_keys  # array of key matrices
-    key_matrix = string_to_matrix(key)
-    round_keys[0] = key_matrix
+def galois_multiplication(A, B, rowA, rowB, colB):
+    result = np.zeros((rowA, colB), dtype=np.uint8)
+    for i in range(rowA):
+        for j in range(colB):
+            for k in range(rowB):
+                result[i][j] ^= A[i][k].gf_multiply_modular(
+                    BitVector(hexstring=hex(B[k][j])[2:]), AES_modulus, 8).intValue()
 
-    for round in range(n_rounds):
-        key_matrix = expand_key(round + 1, key_matrix)
-        round_keys[round + 1] = key_matrix
-
-    return round_keys
+    return result
 
 
 def AES_encryption(plaintext):
     assert len(plaintext) == 16
     state_matrix = string_to_matrix(plaintext)
+    key_matrix = generate_key_matrix(0)
 
     # Round 0
-    state_matrix = np.bitwise_xor(state_matrix, round_keys[0])
+    state_matrix = np.bitwise_xor(state_matrix, key_matrix)
 
     for round in range(n_rounds):
-        key_matrix = round_keys[round + 1]
-        # print("Round", round + 1)
-        # print("State Matrix: ")
-        # print_hex_matrix(state_matrix)
-        # print("Round Key Matrix: ")
-        # print_hex_matrix(key_matrix)
+        key_matrix = generate_key_matrix(round + 1)
+
+        # print_round_details(round + 1, state_matrix, key_matrix)
 
         # Substitute bytes
         state_matrix = substitute_bytes(state_matrix, Sbox)
@@ -128,14 +122,7 @@ def AES_encryption(plaintext):
 
         # Mix Columns
         if round != n_rounds - 1:
-            result = np.zeros((4, 4), dtype=np.uint8)
-            for i in range(4):
-                for j in range(state_matrix.shape[1]):
-                    for k in range(4):
-                        result[i][j] ^= Mixer[i][k].gf_multiply_modular(
-                            BitVector(hexstring=hex(state_matrix[k][j])[2:]), AES_modulus, 8).intValue()
-
-            state_matrix = result
+            state_matrix = galois_multiplication(Mixer, state_matrix, 4, 4, 4)
             # print("After Mix Columns:")
             # print_hex_matrix(state_matrix)
 
@@ -150,19 +137,17 @@ def AES_encryption(plaintext):
 
 
 def AES_decryption(ciphertext):
-    assert len(ciphertext) == 16 and len(key) == 16
+    assert len(ciphertext) == 16
     state_matrix = string_to_matrix(ciphertext)
+    key_matrix = generate_key_matrix(n_rounds)
 
     # Round 0
-    state_matrix = np.bitwise_xor(state_matrix, round_keys[10])
+    state_matrix = np.bitwise_xor(state_matrix, key_matrix)
 
     for round in range(n_rounds):
-        key_matrix = round_keys[9 - round]
-        # print("Round", round + 1)
-        # print("State Matrix: ")
-        # print_hex_matrix(state_matrix)
-        # print("Round Key Matrix: ")
-        # print_hex_matrix(key_matrix)
+        key_matrix = generate_key_matrix(n_rounds - round - 1)
+
+        # print_round_details(round + 1, state_matrix, key_matrix)
 
         # Inverse Shift Rows
         for row in range(state_matrix.shape[0]):
@@ -176,21 +161,17 @@ def AES_decryption(ciphertext):
 
         # Inverse Mix Columns
         if round != n_rounds - 1:
-            result = np.zeros((4, 4), dtype=np.uint8)
-            for i in range(4):
-                for j in range(state_matrix.shape[1]):
-                    for k in range(4):
-                        result[i][j] ^= InvMixer[i][k].gf_multiply_modular(
-                            BitVector(hexstring=hex(state_matrix[k][j])[2:]), AES_modulus, 8).intValue()
-
-            state_matrix = result
+            state_matrix = galois_multiplication(
+                InvMixer, state_matrix, 4, 4, 4)
 
     plaintext = matrix_to_string(state_matrix)
     return plaintext
 
 
 def encrypt(message):
-    assert len(round_keys) == n_rounds + 1
+    if not key_expanded:
+        schedule_key(key)
+
     while len(message) % 16 != 0:
         message += "\0"
 
@@ -203,7 +184,8 @@ def encrypt(message):
 
 
 def decrypt(cipher):
-    assert len(round_keys) == n_rounds + 1
+    if not key_expanded:
+        schedule_key(key)
     blocks = [cipher[i:i + 16] for i in range(0, len(cipher), 16)]
     plaintext = ""
 
@@ -212,43 +194,27 @@ def decrypt(cipher):
     return plaintext
 
 
-def measure_execution_time(func, task_name, *args, **kwargs):
-    import time
-    start = time.time()
-    result = func(*args, **kwargs)
-    end = time.time()
-    print(task_name, "Time:", (end - start) * 1000, "ms")
-    return result
+key = "BUET CSE19 Batch 2023 Fall Semester"
+message = "Never gonna give you up, never gonna let you down, never gonna run around and desert you, never gonna make you cry, never gonna say goodbye, never gonna tell a lie and hurt you"
 
+schedule_key(key)
 
-def print_things(title, str, ascii_first):
-    print(title + ":")
-    if ascii_first:
-        print("In ASCII:", str)
-        print("In HEX: ", end="")
-        print_hex_string(str)
-    else:
-        print("In HEX:", str)
-        print("In ASCII: ", end="")
-        print_hex_string(str)
-    print()
-
-
-key = "BUET CSE19 Batch"
-message = "Never Gonna Give you up"
-
-# key = input("Enter Key: ")[:16]
+# key = input("Enter Key: ")[:32]
 # message = input("Enter Message: ")
 
-print_things("Key", key, True)
-print_things("Plain Text", message, True)
+print_text_details("Key", key, True)
+print_text_details("Plain Text", message, True)
 
 
-measure_execution_time(schedule_key, "Key Schedule", key, n_rounds)
-cipher = measure_execution_time(encrypt, "Encryption", message)
-deciphered = measure_execution_time(decrypt, "Decryption", cipher)
+any, schedule_time = measure_execution_time(schedule_key, "Key Schedule", key)
+cipher, cipher_time = measure_execution_time(encrypt, "Encryption", message)
+deciphered, decipher_time = measure_execution_time(
+    decrypt, "Decryption", cipher)
 
-print()
+print_text_details("Ciphered Text", cipher, False)
+print_text_details("Deciphered Text", deciphered, False)
 
-print_things("Ciphered Text", cipher, False)
-print_things("Deciphered Text", deciphered, False)
+print("Execution Time Details:")
+print("Key Schedule Time:", schedule_time, "ms")
+print("Encryption Time:", cipher_time, "ms")
+print("Decryption Time:", decipher_time, "ms")
